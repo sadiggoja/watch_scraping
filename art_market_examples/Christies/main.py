fc0c29379bbcf541ebb1138d6cf54c6ce0b5a9f1
sadiggoja <sadiqqocayev10@gmail.com>
2022-12-19 23:21:05 +0100

    Complete

--- art_market_examples/Christies/main.py
+++
@@ -1,7 +0,0 @@
-from ChristiesParser import christies_parser
-
-if __name__ == '__main__':
-    christies_parser("Impressionist and Modern", 2019, 2019)
-    christies_parser("Post War Contemporary", 2019, 2019)
-    christies_parser("Asian Contemporary", 2019, 2019)
-    christies_parser("All Contemporary", 2019, 2019)
